ad943834eec625b5a05ffdc0fd5e58bfb1abd460
seongeun <seongeun.cho.dev@gmail.com>
2024-02-19 12:21:13 +0900

    remove unnecessary files

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,3 @@
-import 'package:clone_flutter_youtube/src/navigation/pages.dart';
 import 'package:clone_flutter_youtube/src/route/app_router.dart';
 import 'package:clone_flutter_youtube/src/theme/theme.dart';
 import 'package:flutter/material.dart';
